b91673517fc5b2d3c1ca77605dd6c0d5ca76eb67
Andylaa10 <andylaa100@gmail.com>
2024-03-05 10:10:26 +0100

    addition web api is done

---
+++ Calculator/AdditionService/Controllers/AdditionController.cs
@@ -0,0 +1,33 @@
+using AdditionService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdditionService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AdditionController : ControllerBase
+{
+    private readonly ILogger<AdditionController> _logger;
+    private readonly IAdditionService _additionService;
+
+    public AdditionController(ILogger<AdditionController> logger, IAdditionService additionService)
+    {
+        _logger = logger;
+        _additionService = additionService;
+    }
+
+    [HttpGet]
+    [Route("{number1}/{number2}")]
+    public async Task<IActionResult> Addition([FromRoute] double number1, [FromRoute] double number2)
+    {
+        try
+        {
+            var result = await _additionService.Addition(number1, number2);
+            return Ok(result);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
