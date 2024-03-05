46a7c528404f595f34477c980123010443c9d211
KristianHollaender <90904724+KristianHollaender@users.noreply.github.com>
2024-03-05 11:01:27 +0100

    Started on Telemetry/zipkin + Serilog/Seq

--- Calculator/AdditionService/Services/AdditionService.cs
+++ Calculator/AdditionService/Services/AdditionService.cs
@@ -1,9 +1,15 @@
-﻿namespace AdditionService.Services;
+﻿using Monitoring;
+
+namespace AdditionService.Services;
 
 public class AdditionService : IAdditionService
 {
     public async Task<double> Addition(double number1, double number2)
     {
+        using var activity = MonitorService.ActivitySource.StartActivity();
+        
+        MonitorService.Log.Debug("Called Addition function");
+        
         return await Task.Run(() => number1 + number2);
     }
 }
---
+++ Calculator/Monitoring/MonitorService.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog;
+
+namespace Monitoring;
+
+public class MonitorService
+{
+    public static readonly string ServiceName = Assembly.GetCallingAssembly().GetName().Name ?? "Unknown";
+    public static TracerProvider TracerProvider;
+    public static ActivitySource ActivitySource = new ActivitySource(ServiceName);
+
+    public static ILogger Log => Serilog.Log.Logger;
+    
+    static MonitorService()
+    {
+        // OpenTelemetry
+        TracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddConsoleExporter()
+            .AddZipkinExporter()
+            .AddSource(ActivitySource.Name)
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(ServiceName))
+            .Build();
+        
+        // Serilog
+        Serilog.Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .WriteTo.Console()
+            .WriteTo.Seq("http://localhost:5341")
+            .CreateLogger();
+    }
+}
