2fcde85d1f5638f11b50d823088c47b778012741
KristianHollaender <90904724+KristianHollaender@users.noreply.github.com>
2024-03-05 10:27:21 +0100

    Subtraction api added

---
+++ Calculator/SubtractionService/Services/Interfaces/ISubtractionService.cs
@@ -0,0 +1,6 @@
+﻿namespace SubtractionService.Services.Interfaces;
+
+public interface ISubtractionService
+{
+    public Task<double> Subtraction(double number1, double number2);
+}
---
+++ Calculator/SubtractionService/Services/SubtractionService.cs
@@ -0,0 +1,11 @@
+﻿using SubtractionService.Services.Interfaces;
+
+namespace SubtractionService.Services;
+
+public class SubtractionService : ISubtractionService
+{
+    public async Task<double> Subtraction(double number1, double number2)
+    {
+        return await Task.Run(() => number1 - number2);
+    }
+}
