4cdae0d4a043a976ef5f67ebcc3affbb4162bea9
Andylaa10 <andylaa100@gmail.com>
2024-03-14 23:22:27 +0100

    implemented a mix of fire and hope and fire and forget
    No error handling and no awaiting, if the request fails, we still get the result. But it is not added to the calculationsDb

--- Calculator/AdditionService/Controllers/AdditionController.cs
+++ Calculator/AdditionService/Controllers/AdditionController.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Text.Json;
 using AdditionService.Services;
 using Microsoft.AspNetCore.Mvc;
 using Monitoring;
@@ -10,6 +12,7 @@ public class AdditionController : ControllerBase
 {
     private readonly ILogger<AdditionController> _logger;
     private readonly IAdditionService _additionService;
+    private HttpClient _httpClient = new();
 
     public AdditionController(ILogger<AdditionController> logger, IAdditionService additionService)
     {
@@ -18,12 +21,28 @@ public class AdditionController : ControllerBase
     }
 
     [HttpGet]
-    [Route("{number1}/{number2}")]
-    public async Task<IActionResult> Addition([FromRoute] double number1, [FromRoute] double number2)
+    [Route("{firstNumber}/{secondNumber}")]
+    public async Task<IActionResult> Addition([FromRoute] double firstNumber, [FromRoute] double secondNumber)
     {
         try
         {
-            var result = await _additionService.Addition(number1, number2);
+            var result = await _additionService.Addition(firstNumber, secondNumber);
+
+            //This means addition
+            var operation = 0;
+            var dto = new
+            {
+                firstNumber,
+                secondNumber,
+                result,
+                operation
+            };
+
+            var payload = JsonSerializer.Serialize(dto);
+            var content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+            _httpClient.PostAsync("http://Calculator/api/Calculation/AddCalculation", content);
+
             return Ok(result);
         }
         catch (Exception e)
--- Calculator/SubtractionService/Controllers/SubtractionController.cs
+++ Calculator/SubtractionService/Controllers/SubtractionController.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using SubtractionService.Services.Interfaces;
 
@@ -9,6 +11,8 @@ public class SubtractionController : ControllerBase
 {
     private readonly ILogger<SubtractionController> _logger;
     private readonly ISubtractionService _subtractionService;
+    private HttpClient _httpClient = new();
+
 
 
     public SubtractionController(ILogger<SubtractionController> logger, ISubtractionService subtractionService)
@@ -18,12 +22,28 @@ public class SubtractionController : ControllerBase
     }
 
     [HttpGet]
-    [Route("{number1}/{number2}")]
-    public async Task<IActionResult> Subtraction([FromRoute] double number1, [FromRoute] double number2)
+    [Route("{firstNumber}/{secondNumber}")]
+    public async Task<IActionResult> Subtraction([FromRoute] double firstNumber, [FromRoute] double secondNumber)
     {
         try
         {
-            var result = await _subtractionService.Subtraction(number1, number2);
+            var result = await _subtractionService.Subtraction(firstNumber, secondNumber);
+            
+            //This means subtraction
+            var operation = 1;
+            var dto = new
+            {
+                firstNumber,
+                secondNumber,
+                result,
+                operation
+            };
+
+            var payload = JsonSerializer.Serialize(dto);
+            var content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+            _httpClient.PostAsync("http://Calculator/api/Calculation/AddCalculation", content);
+            
             return Ok(result);
         }
         catch (Exception e)
